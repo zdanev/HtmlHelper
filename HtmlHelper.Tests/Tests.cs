10b7398a0edf751ece8d8c6868269833ce85aef9
Zdravko Danev <zdanev002@nam.ad.pwcinternal.com>
2017-07-31 10:13:31 -0700

    Initial Commit

---
+++ HtmlHelper.Tests/Tests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Xunit;
+
+namespace HtmlHelper.Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1()
+        {
+            // arrange 
+            var tag = new Tag("test");
+
+            // act
+            var html = tag.Render();
+
+            // assert
+            Assert.Equal("<test/>", html);
+        }
+    }
+}
