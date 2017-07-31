542589a4278cf26f1368344c4f7eeb897278942c
zdanev <zdanev@gmail.com>
2017-07-31 10:13:31 -0700

    Initial Commit

---
+++ HtmlHelper/Tag.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HtmlHelper
+{
+    public class Tag
+    {
+        public string Name { get; }
+
+        public Tag(string name)
+        {
+            Name = name;
+        }
+
+        public string Render()
+        {
+            return $"<{Name}/>";
+        }
+    }
+}
