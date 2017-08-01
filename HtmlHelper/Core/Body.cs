98865fd40601ac5d6b9e24e88e30479c1efe155e
zdanev <zdanev@gmail.com>
2017-08-01 08:08:40 -0700

    cleanup

--- HtmlHelper/Core/Body.cs
+++ HtmlHelper/Core/Body.cs
@@ -4,6 +4,20 @@ namespace HtmlHelper
     {
     }
 
+    public abstract class BodyElement : Tag, IBodyElement
+    {
+        public BodyElement(string name, params IHtmlElement[] content) : base(name, content)
+        {
+        }
+
+        public new BodyElement Attr(string name, string value)
+        {
+            Attributes.Add(new HtmlAttribute(name, value));
+            
+            return this;
+        }      
+    }
+    
     public class Body : Tag
     {
         public Body(params IBodyElement[] content) : base("body", content)
